7c17c2bacced07b6c5d8813641f62163cc936d0c
pratikash <pratikashpanda.1234@gmail.com>
2023-12-16 16:50:57 +0530

    create google OAuth

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,11 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default:
+        "https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?w=740&t=st=1702698081~exp=1702698681~hmac=bb02f86f6ae1f3fc4304e746619c680c6ff5d50df7ecb113c969b9c077789281",
+    },
   },
   { timestamps: true }
 );
