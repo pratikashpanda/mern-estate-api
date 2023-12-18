666a99a3787f7c5542da80d89a9ac1a94ab4fd26
pratikash <pratikashpanda.1234@gmail.com>
2023-12-18 12:21:10 +0530

    create signout user api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -50,3 +50,12 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signOutUser = async (req, res, next) =>{
+  try {
+    res.clearCookie('access_token')
+    res.status(200).json("User has been signed out!");
+  } catch (error) {
+    next(error)
+  }
+}
