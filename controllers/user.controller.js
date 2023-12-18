b0301e1df4183d91c1e595f55cad7141a77cff93
pratikash <pratikashpanda.1234@gmail.com>
2023-12-18 12:06:43 +0530

    create delete user api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -33,7 +33,20 @@ export const updateUser = async (req, res, next) => {
 
     res.status(200).json(rest);
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
+
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
+  } catch (error) {
     next(error);
   }
 };
