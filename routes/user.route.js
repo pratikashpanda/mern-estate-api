c4f67532d207cc2be7d21d4e19b86fbbe1ab4ada
pratikash <pratikashpanda.1234@gmail.com>
2023-12-20 21:56:57 +0530

    create get all lisings api

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { test, updateUser, deleteUser, signOutUser } from "../controllers/user.controller.js";
+import {
+  test,
+  updateUser,
+  deleteUser,
+  signOutUser,
+  getUserListings,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,4 +14,5 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/signout", signOutUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 export default router;
