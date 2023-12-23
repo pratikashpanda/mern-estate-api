330bc6d5da8a5ab281a278eec098bf1471c12055
pratikash <pratikashpanda.1234@gmail.com>
2023-12-23 17:53:17 +0530

    create api to contact landlord

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,6 +5,7 @@ import {
   deleteUser,
   signOutUser,
   getUserListings,
+  getUser
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,6 +14,7 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/signout", signOutUser)
-router.get('/listings/:id', verifyToken, getUserListings)
+router.get("/signout", signOutUser);
+router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 export default router;
